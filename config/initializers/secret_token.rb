bb771b996f2e074337b0b1794b411c423bf827d3
aerani <aerani@waffledev.(none)>
2013-03-19 20:48:13 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wabook::Application.config.secret_token = 'bd4c9c754d53fa1073002d3bdfbed2534584c7727834a81575b1855e57ad161db178457f6aad26df4a995006020e53ce78d8f32023c2970c03d0bdab6ec14138'
