c91f56de1c6554f35f6ffbac58ab087b4cc5f2d4
Sineong <sineong@wafflestudio.com>
2013-07-14 16:05:44 +0900

    Mypage

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,5 +1,16 @@
 class MypageController < ApplicationController
 
   def index
+    @checkout_list = Array.new
+
+    @user = current_user
+    @checkout = Checkout.all
+    @checkout.each do |c|
+      if c.user_id == @user.id
+        @book_id = c.book_id
+        @book = Book.all.find_all { |a| a.id == @book_id } 
+        @checkout_list.push(@book[0])
+      end
+    end
   end
 end
